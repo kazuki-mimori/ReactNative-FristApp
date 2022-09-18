104ceeeeaf7f1658f7138375c6bc47c2b461bdd0
mimori <kazumasa071210@gmail.com>
2022-09-18 10:31:40 +0900

    ⬆️

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoListScreens from './src/screens/MemoListScreens';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoListScreens from './src/screens/MemoListScreens';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
